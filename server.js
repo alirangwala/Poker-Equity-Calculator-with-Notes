1e3e24ca39bc1918b9410c1438bd00591905ea44
alirangwala <alirbaba40@gmail.com>
2021-01-08 18:39:35 -0700

    refactor for heroku

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const port = process.env.PORT || 3000;
+const path = require('path')
 const mongoose = require('mongoose');
-const userRouter = require('../database/controllers/users.js')
-const notesRouter = require('../database/controllers/notes.js')
+const userRouter = require('./database/controllers/users.js')
+const notesRouter = require('./database/controllers/notes.js')
+require("dotenv").config()
 
-app.use(express.static('../client/dist')); // Host your dist folder up to the server
+
+app.use(express.static('./client/dist')); // Host your dist folder up to the server
+app.use(express.static(path.join(__dirname, "client", "build")))
+
 app.use(express.json()); // Alternative to BodyParser
 
-mongoose.connect('mongodb://localhost/notes', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/notes' {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   createIndexes: true
@@ -35,9 +40,12 @@ app.use('/notes', notesRouter)
 //   res.status(200).send('The server is taking requests to the products/:id endpoint');
 // });
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 // Listening for requests on the PORT
-app.listen(PORT, () => {
-  console.log('Serving up now at ' + JSON.stringify(PORT))
+app.listen(port, () => {
+  console.log('Serving up now at ' + JSON.stringify(port))
 });
 
 module.exports = db;
